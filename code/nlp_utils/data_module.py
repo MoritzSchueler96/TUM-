940ffe504b4ac697d3f4e5a3088f0f1198376bfd
MoritzSchueler96 <47536226+MoritzSchueler96@users.noreply.github.com>
2021-05-11 10:50:37 +0200

    include utils code as module

--- code/nlp_utils/data_module.py
+++ code/nlp_utils/data_module.py
@@ -7,8 +7,9 @@ from transformers import AutoTokenizer, DistilBertTokenizer
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-from code.config import create_config
+from nlp_utils.config import create_config
 import tldextract
+
 tld_extractor = tldextract.TLDExtract()
 from collections import Counter
 from torchtext.data.utils import get_tokenizer
@@ -20,16 +21,23 @@ class GroupId2Name:
     """
     Helper function to transform facebook group IDs to group names.
     """
+
     def __init__(self, config):
         dirname = os.path.dirname(__file__)
-        filename = os.path.join(dirname, config['dataset_path'])
+        filename = os.path.join(dirname, config["dataset_path"])
         df = pd.read_csv(filename, low_memory=False)
-        self.group_id2name = {x[1]: x[0] for x in df.drop_duplicates(subset=['Group Name', 'Facebook Id'])[['Group Name', 'Facebook Id']].values.tolist()}
-        
+        self.group_id2name = {
+            x[1]: x[0]
+            for x in df.drop_duplicates(subset=["Group Name", "Facebook Id"])[
+                ["Group Name", "Facebook Id"]
+            ].values.tolist()
+        }
+
     def __call__(self, groupId):
         return self.group_id2name[groupId]
 
-def inverse_transform(vector, config = None, encoder = None):
+
+def inverse_transform(vector, config=None, encoder=None):
     """
     returns the actual categories from a one-hot-encoding.
     Based on the config, some categories might be missing (e.g. group_id for rtnews)
@@ -38,16 +46,17 @@ def inverse_transform(vector, config = None, encoder = None):
     result = {}
     decoded = list(encoder.inverse_transform(vector)[0])
 
-    if config['category_group_id']:
-        result['group_id'] = decoded.pop(0)
+    if config["category_group_id"]:
+        result["group_id"] = decoded.pop(0)
 
-    if config['category_type']:
-        result['post_type'] = decoded.pop(0)
+    if config["category_type"]:
+        result["post_type"] = decoded.pop(0)
 
-    if config['category_tld']:
-        result['domain'] = decoded.pop(0)
-    
+    if config["category_tld"]:
+        result["domain"] = decoded.pop(0)
+
     return result
+
 
 def get_classes_per_row(dataset, config):
     """
@@ -55,211 +64,261 @@ def get_classes_per_row(dataset, config):
     """
     for x in dataset:
         result = []
-        if config['category_group_id']:
-            result.append(x['Facebook Id'])
-        
-        if config['category_type']:
-            result.append(x['Type'])
-           
-        if config['category_tld']:
-            domain = ''
-            
-            if not pd.isna(x['Link']):
-                tld_result = tld_extractor(x['Link'])
+        if config["category_group_id"]:
+            result.append(x["Facebook Id"])
+
+        if config["category_type"]:
+            result.append(x["Type"])
+
+        if config["category_tld"]:
+            domain = ""
+
+            if not pd.isna(x["Link"]):
+                tld_result = tld_extractor(x["Link"])
                 domain = tld_result.domain
-                
+
             result.append(domain)
-        
+
         yield tuple(result)
+
 
 class Collator:
     """
     helper class to transform a batch so it can be fed into the DistilBERT based model
     """
+
     def __init__(self, tokenizer, class_encoder, config):
         self.config = create_config(config)
         self.tokenizer = tokenizer
         self.class_encoder = class_encoder
-        
+
     def collate(self, batch):
-        labels, features  = zip(*batch)
-            
-        encoded_texts = self.tokenizer([row['Text'] for row in features], padding=True, truncation=True, return_tensors="pt")
-        encoded_classes = self.class_encoder.transform(list(get_classes_per_row(features, self.config)))
-        
-        return torch.FloatTensor(labels), encoded_texts, torch.FloatTensor(encoded_classes), features
+        labels, features = zip(*batch)
+
+        encoded_texts = self.tokenizer(
+            [row["Text"] for row in features],
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+        )
+        encoded_classes = self.class_encoder.transform(
+            list(get_classes_per_row(features, self.config))
+        )
+
+        return (
+            torch.FloatTensor(labels),
+            encoded_texts,
+            torch.FloatTensor(encoded_classes),
+            features,
+        )
 
 
 class CrowdTangleDataset(Dataset):
     def __init__(self, texts, labels):
         self.texts = texts
         self.labels = labels
-    
+
     def __getitem__(self, index):
         return self.labels[index], self.texts[index]
-    
+
     def __len__(self):
         return len(self.texts)
-    
+
+
 def create_datasets(class_encoder, config={}):
     """
     creates a train, validation and test dataset.
     The trainset will be shuffeled. 42 is used as random seed to get deterministic results.
     """
     config = create_config(config)
-    
+
     dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, config['dataset_path'])
+    filename = os.path.join(dirname, config["dataset_path"])
     df = pd.read_csv(filename, low_memory=False)
-    
-    df['Text'] = df['Message'].fillna('').astype(str) + ' ' + df['Image Text'].fillna('').astype(str) + ' ' + \
-                df['Link Text'].fillna('').astype(str) + ' ' + df['Description'].fillna('').astype(str)
-    
-    raw_records = df.to_dict('records')
-    raw_labels = df[config['column_goldlabel']].to_list()
-    
+
+    df["Text"] = (
+        df["Message"].fillna("").astype(str)
+        + " "
+        + df["Image Text"].fillna("").astype(str)
+        + " "
+        + df["Link Text"].fillna("").astype(str)
+        + " "
+        + df["Description"].fillna("").astype(str)
+    )
+
+    raw_records = df.to_dict("records")
+    raw_labels = df[config["column_goldlabel"]].to_list()
+
     # Split train / val_test date
-    X_train, X_val_test, y_train, y_val_test = train_test_split(raw_records, raw_labels, test_size=0.4, random_state=42)
-    
+    X_train, X_val_test, y_train, y_val_test = train_test_split(
+        raw_records, raw_labels, test_size=0.4, random_state=42
+    )
+
     # initialize encoder using training data
     class_encoder.fit(list(get_classes_per_row(X_train, config)))
-    
-    # Split validation / test set
-    X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
 
-    return CrowdTangleDataset(X_train, y_train), \
-           CrowdTangleDataset(X_val, y_val), \
-           CrowdTangleDataset(X_test, y_test)
-           
-    
+    # Split validation / test set
+    X_val, X_test, y_val, y_test = train_test_split(
+        X_val_test, y_val_test, test_size=0.5, random_state=42
+    )
+
+    return (
+        CrowdTangleDataset(X_train, y_train),
+        CrowdTangleDataset(X_val, y_val),
+        CrowdTangleDataset(X_test, y_test),
+    )
+
+
 class CrowdTangleDataModule(pl.LightningDataModule):
     """
     Data module for the DistilBERT based model + linear model
     """
+
     def __init__(self, num_workers=4, config={}):
         super().__init__()
         config = create_config(config)
-        self.batch_size = config['batch_size']
+        self.batch_size = config["batch_size"]
         self.num_workers = num_workers
-        
+
         self.collator = None
         self.trainset = None
         self.valset = None
         self.testset = None
         self.config = config
-        #self.tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-        self.tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
-        self.class_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False) 
+        # self.tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
+        self.tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
+        self.class_encoder = OneHotEncoder(handle_unknown="ignore", sparse=False)
 
     def setup(self, stage):
         if self.trainset is None:
             self.collator = Collator(self.tokenizer, self.class_encoder, self.config)
-            self.trainset, self.valset, self.testset = create_datasets(self.class_encoder, self.config)
-        
+            self.trainset, self.valset, self.testset = create_datasets(
+                self.class_encoder, self.config
+            )
+
     def train_dataloader(self):
         return DataLoader(
-            self.trainset, 
+            self.trainset,
             shuffle=True,
-            batch_size=self.batch_size, 
-            collate_fn=self.collator.collate, 
-            num_workers=self.num_workers)
-                                               
+            batch_size=self.batch_size,
+            collate_fn=self.collator.collate,
+            num_workers=self.num_workers,
+        )
+
     def val_dataloader(self):
         return DataLoader(
-            self.valset, 
-            batch_size=self.batch_size, 
+            self.valset,
+            batch_size=self.batch_size,
             collate_fn=self.collator.collate,
-            num_workers=self.num_workers)
+            num_workers=self.num_workers,
+        )
 
     def test_dataloader(self):
         return DataLoader(
-            self.testset, 
-            batch_size=self.batch_size, 
+            self.testset,
+            batch_size=self.batch_size,
             collate_fn=self.collator.collate,
-            num_workers=self.num_workers)
-    
+            num_workers=self.num_workers,
+        )
+
     def get_tokenizer(self):
         return self.tokenizer
+
 
 class PlainCollator:
     """
         helper class to transform a batch so it can be fed into the BiLSTM model
         """
+
     def __init__(self, vocab, tokenizer, class_encoder, config):
         self.config = create_config(config)
         self.vocab = vocab
         self.tokenizer = tokenizer
         self.class_encoder = class_encoder
-        
+
     def collate(self, batch):
-        labels, features  = zip(*batch)
-        
+        labels, features = zip(*batch)
+
         encoded_texts = []
         for row in features:
             encoded_texts.append(
-                torch.tensor([self.vocab[token] for token in self.tokenizer(row['Text'])])
+                torch.tensor(
+                    [self.vocab[token] for token in self.tokenizer(row["Text"])]
+                )
             )
-        
-        encoded_classes = self.class_encoder.transform(list(get_classes_per_row(features, self.config)))
-        
-        return torch.FloatTensor(labels),\
-               pad_sequence(encoded_texts, batch_first=True, padding_value=self.vocab['<pad>']),\
-               torch.FloatTensor(encoded_classes),\
-               features
 
-    
+        encoded_classes = self.class_encoder.transform(
+            list(get_classes_per_row(features, self.config))
+        )
+
+        return (
+            torch.FloatTensor(labels),
+            pad_sequence(
+                encoded_texts, batch_first=True, padding_value=self.vocab["<pad>"]
+            ),
+            torch.FloatTensor(encoded_classes),
+            features,
+        )
+
+
 class PlainCrowdTangleDataModule(pl.LightningDataModule):
     """
         Data module for the BiLSTM model
     """
+
     def __init__(self, num_workers=4, config={}):
         super().__init__()
         config = create_config(config)
-        self.batch_size = config['batch_size']
+        self.batch_size = config["batch_size"]
         self.num_workers = num_workers
-        
+
         self.collator = None
         self.trainset = None
         self.valset = None
         self.testset = None
         self.vocab = None
         self.config = config
-        self.tokenizer =  get_tokenizer('basic_english')
-        self.class_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
-        
-    
+        self.tokenizer = get_tokenizer("basic_english")
+        self.class_encoder = OneHotEncoder(handle_unknown="ignore", sparse=False)
+
     def setup(self, stage):
         if self.trainset is None:
-            self.trainset, self.valset, self.testset = create_datasets(self.class_encoder, self.config)
+            self.trainset, self.valset, self.testset = create_datasets(
+                self.class_encoder, self.config
+            )
             self._build_vocab()
-            self.collator = PlainCollator(self.vocab, self.tokenizer, self.class_encoder, self.config)
-    
+            self.collator = PlainCollator(
+                self.vocab, self.tokenizer, self.class_encoder, self.config
+            )
+
     def _build_vocab(self):
         counter = Counter()
         for (label, features) in iter(self.trainset):
-            counter.update(self.tokenizer(features['Text']))
-        self.vocab = Vocab(counter, min_freq=self.config['vocab_min_freq'])
-    
-            
+            counter.update(self.tokenizer(features["Text"]))
+        self.vocab = Vocab(counter, min_freq=self.config["vocab_min_freq"])
+
     def train_dataloader(self):
         return DataLoader(
-            self.trainset, 
+            self.trainset,
             shuffle=True,
-            batch_size=self.batch_size, 
-            collate_fn=self.collator.collate, 
-            num_workers=self.num_workers)
-                                               
+            batch_size=self.batch_size,
+            collate_fn=self.collator.collate,
+            num_workers=self.num_workers,
+        )
+
     def val_dataloader(self):
         return DataLoader(
-            self.valset, 
-            batch_size=self.batch_size, 
+            self.valset,
+            batch_size=self.batch_size,
             collate_fn=self.collator.collate,
-            num_workers=self.num_workers)
+            num_workers=self.num_workers,
+        )
 
     def test_dataloader(self):
         return DataLoader(
-            self.testset, 
-            batch_size=self.batch_size, 
+            self.testset,
+            batch_size=self.batch_size,
             collate_fn=self.collator.collate,
-            num_workers=self.num_workers)
-        
+            num_workers=self.num_workers,
+        )
+
--- code/nlp_utils/model.py
+++ code/nlp_utils/model.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from transformers import DistilBertModel, BertModel
-from code.config import create_config
+from nlp_utils.config import create_config
 
 
 class BaseModel(pl.LightningModule):
@@ -11,166 +11,202 @@ class BaseModel(pl.LightningModule):
     This is the DistilBERT based model. It's called "BaseModel" as I thought it would
     be the only NN model we would make...
     """
-    
-    def __init__(self, config = {}):
-        
+
+    def __init__(self, config={}):
+
         super().__init__()
         config = create_config(config)
         self.config = config
         self.save_hyperparameters(self.config)
-        self.learning_rate = config['learning_rate']
-        
+        self.learning_rate = config["learning_rate"]
+
         self.train_metric = pl.metrics.MeanSquaredError()
         self.val_metric = pl.metrics.MeanSquaredError()
         self.test_metric = pl.metrics.MeanSquaredError()
-        
+
         # setup layers
-        self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
-        
+        self.bert = DistilBertModel.from_pretrained("distilbert-base-uncased")
+
         # freeze the encode, head layer will still be trainable
         for param in self.bert.parameters():
             param.requires_grad = False
-            
+
         # Model design
         self.distilbert_tail = nn.Sequential(
             nn.Linear(self.bert.config.dim, self.bert.config.dim),
             nn.ReLU(),
-            nn.Dropout(self.bert.config.seq_classif_dropout)
+            nn.Dropout(self.bert.config.seq_classif_dropout),
         )
-        
-        
+
         self.category_encoder = nn.Sequential(
-            nn.Linear(config['category_encoded_length'], config['category_encoder_out']), 
-            nn.ReLU()
+            nn.Linear(
+                config["category_encoded_length"], config["category_encoder_out"]
+            ),
+            nn.ReLU(),
         )
         # 768 bert hidden state shape + category_encoder_out
-        self.classifier = nn.Linear(self.bert.config.hidden_size + config['category_encoder_out'], 1) 
-        
-        
+        self.classifier = nn.Linear(
+            self.bert.config.hidden_size + config["category_encoder_out"], 1
+        )
+
     def forward(self, encoded_text, category_vectors):
-        bert_output = self.bert(encoded_text['input_ids'], encoded_text['attention_mask'])
-        
+        bert_output = self.bert(
+            encoded_text["input_ids"], encoded_text["attention_mask"]
+        )
+
         hidden_state = bert_output[0]  # (bs, seq_len, dim)
         pooled_output = hidden_state[:, 0]  # (bs, dim)
         pooled_output = self.distilbert_tail(pooled_output)
-        
+
         categories_encoded = self.category_encoder(category_vectors)
-        #test = torch.cat((pooled_output, categories_encoded))
-        concat = torch.cat((pooled_output, categories_encoded),1)
-              
-        
+        # test = torch.cat((pooled_output, categories_encoded))
+        concat = torch.cat((pooled_output, categories_encoded), 1)
+
         out = self.classifier(concat)
-        #out = self.output_layer(bert_output['pooler_output'])
-        return out;
-    
+        # out = self.output_layer(bert_output['pooler_output'])
+        return out
+
     def training_step(self, batch, batch_idx):
         y, encoded_texts, category_vectors, _ = batch
-        y, encoded_texts, category_vectors = y.to(self.device), encoded_texts.to(self.device), category_vectors.to(self.device)
-        
+        y, encoded_texts, category_vectors = (
+            y.to(self.device),
+            encoded_texts.to(self.device),
+            category_vectors.to(self.device),
+        )
+
         y_hat = self(encoded_texts, category_vectors)
-        
+
         loss = F.mse_loss(y_hat.view(-1), y.view(-1))
         self.train_metric(y_hat, y.unsqueeze(1))
-        
-        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        return {'loss': loss}
-    
+
+        self.log(
+            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
+        )
+        return {"loss": loss}
+
     def training_epoch_end(self, outs):
-        self.log('train_epoch' + type(self.train_metric).__name__, self.train_metric.compute())
-    
+        self.log(
+            "train_epoch" + type(self.train_metric).__name__,
+            self.train_metric.compute(),
+        )
+
     def validation_step(self, batch, batch_idx):
         y, encoded_texts, category_vectors, _ = batch
-        y, encoded_texts, category_vectors = y.to(self.device), encoded_texts.to(self.device), category_vectors.to(self.device)
-        
+        y, encoded_texts, category_vectors = (
+            y.to(self.device),
+            encoded_texts.to(self.device),
+            category_vectors.to(self.device),
+        )
+
         y_hat = self(encoded_texts, category_vectors)
-        
+
         loss = F.mse_loss(y_hat, y.unsqueeze(1))
         self.val_metric(y_hat, y.unsqueeze(1))
-        
-        self.log('val_loss', loss)
-        return {'val_loss': loss}
-        
-        
+
+        self.log("val_loss", loss)
+        return {"val_loss": loss}
+
     def validation_epoch_end(self, outputs):
-        self.log('val_epoch_' + type(self.train_metric).__name__ , self.val_metric.compute())
-        
-    
-   
+        self.log(
+            "val_epoch_" + type(self.train_metric).__name__, self.val_metric.compute()
+        )
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
-    
+
+
 class BiLSTMModel(pl.LightningModule):
     """
     This is the second neural model. (BiLSTM)
     """
-    def __init__(self, config = {}):
+
+    def __init__(self, config={}):
         super().__init__()
-        
+
         config = create_config(config)
         self.config = config
         self.save_hyperparameters(self.config)
-        self.learning_rate = config['learning_rate']
-        
+        self.learning_rate = config["learning_rate"]
+
         self.train_metric = pl.metrics.MeanSquaredError()
         self.val_metric = pl.metrics.MeanSquaredError()
-        
+
         # Network structure
-        self.embedding = nn.Embedding(config['vocab_size'], config['embedding_dim'])
-        
+        self.embedding = nn.Embedding(config["vocab_size"], config["embedding_dim"])
+
         self.category_encoder = nn.Sequential(
-            nn.Linear(config['category_encoded_length'], config['category_encoder_out']), 
-            nn.ReLU()
+            nn.Linear(
+                config["category_encoded_length"], config["category_encoder_out"]
+            ),
+            nn.ReLU(),
         )
-        
-        self.bilstm = nn.LSTM(input_size=config['embedding_dim'], hidden_size=config['bilstm_hidden_dim'], bidirectional=True, batch_first=True)
-        
-        self.classifier = nn.Linear(config['bilstm_hidden_dim']*2 + config['category_encoder_out'], 1) #
-        
+
+        self.bilstm = nn.LSTM(
+            input_size=config["embedding_dim"],
+            hidden_size=config["bilstm_hidden_dim"],
+            bidirectional=True,
+            batch_first=True,
+        )
+
+        self.classifier = nn.Linear(
+            config["bilstm_hidden_dim"] * 2 + config["category_encoder_out"], 1
+        )  #
+
     def forward(self, encoded_texts, encoded_classes):
         embeddings = self.embedding(encoded_texts)
         lstm_out, _ = self.bilstm(embeddings)
         categories_encoded = self.category_encoder(encoded_classes)
-        #print("categories_encoded", categories_encoded.shape)
-        #concat = torch.cat((lstm_out, categories_encoded),1)
-        forward = lstm_out[:, lstm_out.shape[1]-1, :]
-        #backward = lstm_out[:, 0, :]
-        combined = torch.cat((forward,categories_encoded),1)
+        # print("categories_encoded", categories_encoded.shape)
+        # concat = torch.cat((lstm_out, categories_encoded),1)
+        forward = lstm_out[:, lstm_out.shape[1] - 1, :]
+        # backward = lstm_out[:, 0, :]
+        combined = torch.cat((forward, categories_encoded), 1)
         out = self.classifier(combined)
         return out
-    
-    
+
     def training_step(self, batch, batch_idx):
         y, encoded_texts, encoded_classes, _ = batch
-        y, encoded_texts, encoded_classes = y.to(self.device), encoded_texts.to(self.device), encoded_classes.to(self.device)
-        
+        y, encoded_texts, encoded_classes = (
+            y.to(self.device),
+            encoded_texts.to(self.device),
+            encoded_classes.to(self.device),
+        )
+
         y_hat = self(encoded_texts, encoded_classes)
-        
+
         loss = F.mse_loss(y_hat, y.unsqueeze(1))
         self.train_metric(y_hat, y.unsqueeze(1))
-        
-        self.log('loss', loss)
-        return {'loss': loss}
-    
+
+        self.log("loss", loss)
+        return {"loss": loss}
+
     def training_epoch_end(self, outs):
-        self.log('train_epoch' + type(self.train_metric).__name__, self.train_metric.compute())
-    
-    
+        self.log(
+            "train_epoch" + type(self.train_metric).__name__,
+            self.train_metric.compute(),
+        )
+
     def validation_step(self, batch, batch_idx):
         y, encoded_texts, encoded_classes, _ = batch
-        y, encoded_texts, encoded_classes = y.to(self.device), encoded_texts.to(self.device), encoded_classes.to(self.device)
-        
+        y, encoded_texts, encoded_classes = (
+            y.to(self.device),
+            encoded_texts.to(self.device),
+            encoded_classes.to(self.device),
+        )
+
         y_hat = self(encoded_texts, encoded_classes)
-        
+
         loss = F.mse_loss(y_hat, y.unsqueeze(1))
         self.val_metric(y_hat, y.unsqueeze(1))
-        
-        self.log('val_loss', loss)
-        return {'val_loss': loss}
-        
-        
+
+        self.log("val_loss", loss)
+        return {"val_loss": loss}
+
     def validation_epoch_end(self, outputs):
-        self.log('val_epoch_' + type(self.train_metric).__name__ , self.val_metric.compute())
+        self.log(
+            "val_epoch_" + type(self.train_metric).__name__, self.val_metric.compute()
+        )
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
